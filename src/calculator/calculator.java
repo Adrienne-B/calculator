9823aa099ac5dd5193d896ecc363066a3350dede
adrienne_b <WorldofArts_95@yahoo.com>
2022-04-11 21:15:43 -0500

    pushing calculator commit.

---
+++ src/calculator/calculator.java
@@ -0,0 +1,37 @@
+package calculator;
+
+public class calculator {
+		int result;
+		//Addition Method
+		
+	public int add(int num1, int num2){
+		private int result;	
+			result = num1+num2;
+			return result;
+		}
+		//Subtraction Method
+		public int substract(int num1, int num2){
+			result = num1-num2;
+			return result;
+		}
+		//Multiplication Method
+		public int multiply(int num1, int num2){
+			result = num1*num2;
+			return result;
+		}
+		//Division Method
+		public int divide(int num1, int num2){
+			result = num1/num2;
+			return result;
+		}
+		
+		//Square Method
+		public int square(int num1, int num2) {
+			result = num1/num2;
+			return result;
+		}
+	}
+	
+		public static void main(String[] args) {
+			
+		}
